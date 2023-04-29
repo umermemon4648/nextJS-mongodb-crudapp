53926beee118cb290c28686fb90e76578b3c8a0d
umermemon4648 <umermemon4648@gmail.com>
2023-04-30 04:37:21 +0500

    Initial setup for nextjs with bootstrap

---
+++ src/pages/cart.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DisplayCart from '@/components/DisplayCart'
+const cart = () => {
+  return (
+    <>
+    
+    <DisplayCart/>
+    </>
+  )
+}
+
+export default cart
