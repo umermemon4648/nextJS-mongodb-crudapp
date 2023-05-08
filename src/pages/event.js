7b614db0d162c7694843e57ebcc00455a06f0a0b
umermemon4648 <umermemon4648@gmail.com>
2023-05-08 12:38:27 +0500

    Add event form component

--- src/pages/event.js
+++ src/pages/event.js
@@ -1,46 +1,79 @@
 import React, {useState, useEffect} from 'react'
-
+import axios from 'axios';
 const event = () => {
-  const [eventState, setEventState] = useState[{
-    dateInput,
-    eventText,
-    startTime,
-    endTime,
-    radioInput,
-  }]
+  const [eventState, setEventState] = useState({
+    dateInput: "",
+    eventText: "",
+    startTime: "",
+    endTime: "",
+    radioInput: "1 room",
+  })
 
 
+  const getFormData = (e) => {
+    e.preventDefault()
+    const { name, value } = e.target;
+        setEventState({ ...eventState, [name]: value });
+  };
+
+  const registerEvent = async(e)=>{
+    e.preventDefault()
+    const {eventText, dateInput, startTime, endTime} = eventState
+    try{
+
+      if(eventText || dateInput || startTime || endTime){
+        // const {data} = await axios.post('api/event/new',eventState)
+        let res = await fetch('http://localhost:3000/api/event/new',{
+          method: "POST",
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(eventState)
+    
+        })
+        let data = await res.json()
+
+        console.log("data: ", data)
+      }
+
+      
+    }
+    catch(error){
+      console.log(error);
+    }
+  }
+  
   useEffect(()=>{
 
-    
+    console.log(eventState)
+    // registerEvent()
   },[eventState])
 
 
 
   return (
     <>
-      
+      <div>
+        <form onSubmit={registerEvent} method='post'>
     <div className='mb-4 mt-4 px-4'>
       <label>Date</label>
-      <input type="date" name="dateInput" />
+      <input onChange={getFormData} value={eventState.dateInput} type="date" name="dateInput" />
     </div>
 
 
     <div className='mb-4 px-4'>
       <label>Event</label>
-      <input type="text" name="eventText" />
+      <input onChange={getFormData} value={eventState.eventText} type="text" name="eventText" />
     </div>
 
 
     <div className='mb-4 px-4'>
       <label>Start Time</label>
-      <input type="time" name="startTime" />
+      <input onChange={getFormData} value={eventState.startTime} type="time" name="startTime" />
     </div>
 
     
     <div className='mb-4 px-4'>
       <label>End Time</label>
-      <input type="time" name="endTime" />
+      <input onChange={getFormData} value={eventState.endTime} type="time" name="endTime" />
     </div>
     
     <div className='mb-4 px-4'>
@@ -49,27 +82,41 @@ const event = () => {
         Choose a room
       <li>
         
-        <label><input
-
-            name="radioInput"
-            type="radio"
-            className="radio"
-            defaultChecked/>
+        <label>
+        <input
+    onChange={getFormData}
+    type="radio"
+    name="radioInput"
+    className="radio"
+    value="1 room"
+    defaultChecked
+  />
           1 room</label
         >
       </li>
       <li>
-        <label><input
-            name="radioInput"
-            type="radio"
-            className="radio"/>
+        <label>
+        <input
+    onChange={getFormData}
+    type="radio"
+    name="radioInput"
+    className="radio"
+    value="2 room"
+  />
             2 room</label
         >
       </li>
       </ul>
-      
     </div>
 
+
+
+<div>
+  <button  type="submit">Submit</button>
+</div>
+
+    </form>
+    </div>
   </>
   )
 }
